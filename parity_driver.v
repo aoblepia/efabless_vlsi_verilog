d1f817f1913a0878640d9d49d20a32762556f4c0
aoblepia <116849986+aoblepia@users.noreply.github.com>
2023-12-10 14:40:16 -0500

    logic now working: thanks leo

--- parity_driver.v
+++ parity_driver.v
@@ -1,51 +1,56 @@
 module parity_driver;
+   
     reg clk;
     wire busy;
     reg start;
     wire [7:0] data_in;
     wire even_parity;
     wire odd_parity;
-	
-	// Generate the Stimulus 
+   
+    // Generate the Stimulus
     parity_stim stim(data_in);
-	
-	// Test the circuit using the stimulus 
+   
+    // Test the circuit using the stimulus
     parity p0(clk, start, data_in, even_parity, odd_parity, busy);
 
-	always #5 clk = ~clk;
 
-	initial begin
-		start = 1;
-		clk = 0;
-		       
-		#100;
-		start = 0;
-		        
-		#100;
-		start = 1;
-		     
-		#100;
-		start = 0;
-		      
-		#100;
-		start = 1;
+    always #5 clk = ~clk;
 
-		#100;
-		start = 0;
 
-		#100;
-		start = 1;
+    initial begin
+        start = 1;
+        clk = 0;
+               
+        #150;
+        start = 0;
+               
+        #150;
+        start = 1;
+             
+        #150;
+        start = 0;
+             
+        #150;
+        start = 1;
 
-		#100;
-		start = 0;
-		      
-		#10;
-		while (busy)
-			#10;
-			start = ~start;
-		#10;
-		//$monitor ("@ time=%d clk=%b, start=%b, sw_in=%b, seq_num=%b, num=%b, busy=%b", $time, clk, start, sw_in, seq_num, num, busy);
-        $monitor ("@ time=%d clk=%b, start=%b, data_in=%b, even_parity=%b, odd_parity=%b, busy=%b", $time, clk, start, data_in, even_parity, odd_parity, busy);
-		$stop;
+
+        #150;
+        start = 0;
+
+
+        #150;
+        start = 1;
+
+
+        #150;
+        start = 0;
+             
+        #10;
+        while (busy)
+            #10;
+            start = ~start;
+        #10;
+                $monitor ("@ time=%d clk=%b, start=%b, data_in=%b, even_parity=%b, odd_parity=%b, busy=%b", $time, clk, start, data_in, even_parity, odd_parity, busy);
+        $stop;
    end
 endmodule
--- parity_stim.v
+++ parity_stim.v
@@ -2,8 +2,10 @@ module parity_stim (
     output reg [7:0] data_in
 );
 
-   parameter delay = 200;
+
+   parameter delay = 300;
    reg [3:0] count;
+
 
    initial begin
       count = 0;
@@ -13,17 +15,21 @@ module parity_stim (
                   data_in <= 8'b11111101;
             end
 
+
             4'b0001: begin
-                  data_in <= 8'b00001100;
+                  data_in <= 8'b00011100;
             end
 
+
             4'b0010: begin
-                  data_in <= 8'b01100100;
+                  data_in <= 8'b01000100;
             end
+
 
             4'b0011: begin
                   data_in <= 8'b11111111;
             end
+
 
             4'b0100: begin
                   $stop;
@@ -32,5 +38,6 @@ module parity_stim (
          #delay count=count+1;
       end
    end
+
 
 endmodule
